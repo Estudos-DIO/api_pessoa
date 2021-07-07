f9d64bb6895af1094324354481ff59081bb0a504
Mauro Salomão <oruamsalomao@gmail.com>
2021-07-07 19:50:25 -0300

    Implementação dos DTOs de pessoa e telefone.

--- src/main/java/dio/API_Pessoa/dto/PessoaDTO.java
+++ src/main/java/dio/API_Pessoa/dto/PessoaDTO.java
@@ -1,2 +1,40 @@
-package dio.API_Pessoa.dto;public class PessoaDTO {
+package dio.API_Pessoa.dto;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PessoaDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size( min = 2, max = 50 )
+    private String nome;
+
+    @NotNull
+    @Size( min = 2, max = 50 )
+    private String sobrenome;
+
+    @NotNull
+    private String cpf;
+
+    private String dataNascimento;
+
+    @Valid
+    @NotEmpty
+    private List<TelefoneDTO> telefones;
+
 }
--- src/main/java/dio/API_Pessoa/dto/TelefoneDTO.java
+++ src/main/java/dio/API_Pessoa/dto/TelefoneDTO.java
@@ -1,2 +1,26 @@
-package dio.API_Pessoa.dto;public class TelefoneDTO {
+package dio.API_Pessoa.dto;
+
+import com.sun.istack.NotNull;
+import dio.API_Pessoa.enums.TipoTelefone;
+import lombok.*;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TelefoneDTO {
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TipoTelefone tipoTelefone;
+
+    @NotNull
+    @Size(min=13, max=14)
+    private String numero;
+
 }
